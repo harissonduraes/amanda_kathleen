c3a88db68725c938749615f290f69e69441f9571
harisson.duraes <harisson.duraes@ATA-TI58>
2025-07-03 10:56:26 -0300

    Comenta site e whatsapp

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,31 @@ function App() {
     <>
       <div className="links">
         <div>
-        <a href="https://www.youtube.com/@AmandaKathleen15">
-        <button>
-        Youtube
-        </button>
-        </a>
+          <a href="https://www.youtube.com/@AmandaKathleen15">
+            <button>
+              Youtube
+            </button>
+          </a>
         </div>
-      <div>
+        {/* <div>
       <a href="https://wa.me/qr/Z23G7IYLE7QOJ1">
       <button>
       Whatsapp
       </button>
       </a>
-      </div>
-      <div>
-      <a href="https://www.instagram.com/amanda_kathleen15/">
-      <button>
-      Instagram
-      </button>
-      </a>
-      </div>
-      <div>
+      </div> */}
+        <div>
+          <a href="https://www.instagram.com/amanda_kathleen15/">
+            <button>
+              Instagram
+            </button>
+          </a>
+        </div>
+        {/* <div>
       <button>
       Site
       </button>
-      </div>
+      </div> */}
       </div>
     </>
   )
